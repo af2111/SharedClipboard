a952aa9f29f3a96863d34278ab923c35aee45bbc
Martin Fritz <af@martinfritz.net>
2020-08-16 23:26:06 +0200

    added subscribing feature, comments and more

--- mqtt/Program.cs
+++ mqtt/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 using System.Windows.Forms;
-
+using System.IO;
 
 namespace mqtt
 {
@@ -16,19 +16,19 @@ namespace mqtt
         static void Main(string[] args)
         {
             StartProgram();
-            
-            
+
+
         }
         [STAThread]
-        
+
         static void StartProgram()
         {
             Console.WriteLine("Geben sie start ein, um das Program zu Starten.");
             String StartCmd = Console.ReadLine();
-            if(StartCmd == "start")
+            if (StartCmd.ToLower() == "start")
             {
-                Console.WriteLine("Zwischenablage Geteilt! Geben sie erneut \"start\" ein, um die Zwischenablage erneut zu teilen!");
-                PublishClipboard();
+
+                initClipboard();
                 StartProgram();
             }
 
@@ -38,19 +38,48 @@ namespace mqtt
                 StartProgram();
             }
         }
-        static void PublishClipboard()
+        [STAThread]
+        static void initClipboard()
         {
+            
+            // Clipboard bekommen
             String text = Clipboard.GetText();
+
+            // Connecten
             MqttClient client = new MqttClient("localhost");
             byte code = client.Connect(Guid.NewGuid().ToString());
+
+            Console.WriteLine("Geben sie \"get\" oder \"post\" ein");
+            string GetPostCmd = Console.ReadLine();
+            if(GetPostCmd.ToLower() == "get")
+            {
+                // Zum Topic Subscriben
+                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                ushort msgIdSub = client.Subscribe(new string[] { "zwischenablage/zwischenablage" },
+                new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            }
+            else if(GetPostCmd.ToLower() == "post")
+            {
+                // Clipboard publishen
+                ushort msgIdPub = client.Publish("zwischenablage/zwischenablage",
+                Encoding.UTF8.GetBytes(text),
+                MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE,
+                false);
+            }
+           
+            
+            
             
 
-
-            ushort msgId = client.Publish("zwischenablage/zwischenablage", // topic
-                           Encoding.UTF8.GetBytes(text), // message body
-                           MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, // QoS level
-                           false); // retained
         }
-       
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            String msgReceived = Encoding.UTF8.GetString(e.Message);
+            Clipboard.SetText(msgReceived);
+            Console.WriteLine(msgReceived);
+        }
+
+
+
     }
 }
